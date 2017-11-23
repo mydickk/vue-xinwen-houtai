7785b28fa192c8491fa600bdcfa1cb06222913ce
mydickk <269358094@qq.com>
2017-11-23 18:50:05 +0800

    update

--- frontend/src/components/page/Menu.vue
+++ frontend/src/components/page/Menu.vue
@@ -1,66 +1,86 @@
 <template>
-  <el-tree
-    :data="data4"
-    :props="defaultProps"
-    show-checkbox
-    node-key="id"
-    default-expand-all
-    :expand-on-click-node="false"
-    :render-content="renderContent">
-  </el-tree>
+  <div>
+    <el-tree
+      :data="sidebar"
+      :props="defaultProps"
+      node-key="index"
+      default-expand-all
+      :expand-on-click-node="false"
+      :render-content="renderContent">
+    </el-tree>
+
+    <el-dialog :title="editorType.type==='add'?'添加菜单':`修改菜单《${editorType.title}》`" :visible.sync="dialogFormVisible">
+      <el-form :model="activeMenu">
+        <el-form-item label="标题" :label-width="formLabelWidth">
+          <el-input v-model="activeMenu.title" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="路由" :label-width="formLabelWidth">
+          <el-input v-model="activeMenu.index" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图标" :label-width="formLabelWidth">
+          <el-input v-model="activeMenu.icon" auto-complete="off" :disabled="editorType.haveRouter==0"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="confirmChg">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
-
 <script>
-  let id = 1000;
+  import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
 
+  let id = 1000;
   export default {
     data () {
-      console.log(this.$store.state.sidebar);
       return {
-        data4: [{
-          id: 1,
-          label: '一级 1',
-          children: [{
-            id: 4,
-            label: '二级 1-1',
-            children: [{
-              id: 9,
-              label: '三级 1-1-1'
-            }, {
-              id: 10,
-              label: '三级 1-1-2'
-            }]
-          }]
-        }, {
-          id: 2,
-          label: '一级 2',
-          children: [{
-            id: 5,
-            label: '二级 2-1'
-          }, {
-            id: 6,
-            label: '二级 2-2'
-          }]
-        }, {
-          id: 3,
-          label: '一级 3',
-          children: [{
-            id: 7,
-            label: '二级 3-1'
-          }, {
-            id: 8,
-            label: '二级 3-2'
-          }]
-        }],
         defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
+          children: 'subs',
+          label: 'title',
+          id: 'index'
+        },
+        dialogFormVisible: false,
+        activeMenu: {},
+        editorType: {},
+        formLabelWidth: '120px'
       };
     },
 
+    filters: {
+      filterSidebar: function (sidebarData) {
+        sidebarData.forEach(function (elem, index) {
+          if (elem.subs) {
+            elem.subs.forEach(function (e, i) {
+              e._id = i;
+            });
+          };
+        });
+        return sidebarData;
+      }
+    },
+
+    computed: {
+      ...mapState({
+        sidebar: state => state.sidebar
+      }),
+      ...mapGetters([
+        'chgSidebar',
+        'getUserList'
+      ])
+    },
+
     methods: {
+      ...mapMutations([
+        'setSidebar',
+        'setUserList'
+      ]),
+
+      ...mapActions([
+        'fetchSideBar'
+      ]),
+
       append (data) {
         const newChild = { id: id++, label: 'testtest', children: [] };
         if (!data.children) {
@@ -70,22 +90,84 @@
       },
 
       remove (node, data) {
-        const parent = node.parent;
-        const children = parent.data.children || parent.data;
-        const index = children.findIndex(d => d.id === data.id);
-        children.splice(index, 1);
+        this.$confirm(`确认要删除《${data.title}》？`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          const parent = node.parent;
+          const children = parent.data.subs || parent.data;
+          const index = children.findIndex(d => d.index === data.index);
+          children.splice(index, 1);
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {});
+      },
+
+      chgMenu (type, node, data) {
+        this.editorType.type = type;
+        this.editorType.title = data.title;
+        this.editorType.haveRouter = data.subs ? 1 : 0;
+        this.editorType.data = data;
+        this.activeMenu = {};
+        if (type === 'add') { // 该节点的父对象
+          this.editorType.parent = node.parent.data.subs || node.parent.data;
+        } else {
+          for (let key in data) {
+            this.activeMenu[key] = data[key];
+          };
+          this.activeMenu.origin = data;
+        }
+        this.dialogFormVisible = true;
+      },
+
+      confirmChg () {
+        if (this.checkForm()) {
+          if (this.editorType.type === 'add') { // 添加菜单
+            this.editorType.parent.push(this.activeMenu);
+          } else { // 修改菜单
+            for (let key in this.activeMenu) {
+              if (key !== 'origin') {
+                this.activeMenu.origin[key] = this.activeMenu[key];
+              }
+            };
+          };
+          this.dialogFormVisible = false;
+        };
+      },
+
+      checkForm () {
+        if (!this.activeMenu.index) {
+          this.$message({
+            type: 'warning',
+            message: '路由不能为空!'
+          });
+          return false;
+        };
+        if (!this.activeMenu.title) {
+          this.$message({
+            type: 'warning',
+            message: '标题不能为空!'
+          });
+          return false;
+        };
+        return true;
       },
 
       renderContent (h, { node, data, store }) {
+        let Btn = (<span style="font-size: 12px;">
+          <el-button type="text" on-click={ () => { this.chgMenu('add', node, data); } }>添加</el-button>
+          <el-button type="text" on-click={ () => { this.chgMenu('chg', node, data); } }>修改</el-button>
+          <el-button type="text" on-click={ () => this.remove(node, data) }>删除</el-button>
+        </span>);
         return (
           <span style="flex: 1; display: flex; align-items: center; justify-content: space-between; font-size: 14px; padding-right: 8px;">
             <span>
               <span>{node.label}</span>
             </span>
-            <span>
-              <el-button style="font-size: 12px;" type="text" on-click={ () => this.append(data) }>Append</el-button>
-              <el-button style="font-size: 12px;" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button>
-            </span>
+            {Btn}
           </span>);
       }
     },
